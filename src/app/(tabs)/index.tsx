ca46ac9304c74b9495f484a877deafc80081b7d3
Waseem Kurne <kurne.waseem@gmail.com>
2025-11-14 07:07:37 +0530

    flatlist to render

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 import MediaListItem from "@/components/MediaListItem";
 import mediaList from "@assets/data/mediaList.json";
 
 export default function HomeScreen() {
   return (
     <View>
-      <MediaListItem mediaItem={mediaList[0]} />
-      <MediaListItem mediaItem={mediaList[1]} />
-      <MediaListItem mediaItem={mediaList[2]} />
+      <FlatList
+        data={mediaList}
+        renderItem={({ item }) => <MediaListItem mediaItem={item} />}
+      />
     </View>
   );
 }
