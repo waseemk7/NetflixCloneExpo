0076d24b7b875efa2b7c9a446f8bf5acc3a123ab
Waseem Kurne <kurne.waseem@gmail.com>
2025-11-16 13:30:00 +0530

    expo video installed

--- src/app/mediaDetails/[id].tsx
+++ src/app/mediaDetails/[id].tsx
@@ -4,6 +4,8 @@ import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import mediaDetailedList from "@assets/data/mediaDetailedList.json";
 import MediaInfo from "@/components/MediaDetails/MediaInfo";
+import { useVideoPlayer } from "expo-video";
+import MediaHeader from "@/components/MediaDetails/MediaHeader";
 
 export default function MediaDetails() {
   const { id } = useLocalSearchParams();
@@ -34,8 +36,18 @@ export default function MediaDetails() {
     return <Text>No playable video found.</Text>;
   }
 
+  const trailerPlayer = useVideoPlayer(trailer, (player) => {
+    player.currentTime = 10;
+    player.play();
+  });
+
+  const mediaPlayer = useVideoPlayer(videoSource, (player) => {
+    player.showNowPlayingNotification = true;
+  });
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <MediaHeader/>
       <MediaInfo
         title={title}
         releaseYear={releaseYear}
---
+++ src/components/MediaDetails/MediaHeader.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function MediaHeader() {
+  return (
+    <View>
+      <Text style={{color: 'white'}}>MediaHeader</Text>
+    </View>
+  )
+}
