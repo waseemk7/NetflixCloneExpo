b877e4f88475f258450a9834aff37d1b59cafe52
Waseem Kurne <kurne.waseem@gmail.com>
2025-11-16 13:14:30 +0530

    Media Info Component

--- src/app/mediaDetails/[id].tsx
+++ src/app/mediaDetails/[id].tsx
@@ -1,12 +1,50 @@
 import React from "react";
-import { View, Text } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import mediaDetailedList from "@assets/data/mediaDetailedList.json";
+import MediaInfo from "@/components/MediaDetails/MediaInfo";
 
 export default function MediaDetails() {
   const { id } = useLocalSearchParams();
+
+  const mediaItem = mediaDetailedList.find((media) => media.id === id);
+
+  if (!mediaItem) {
+    return <Text>Media Not Found!</Text>;
+  }
+
+  const {
+    type,
+    title,
+    description,
+    releaseYear,
+    ageRestriction,
+    duration,
+    thumbnail,
+    trailer,
+    videoUrl,
+    seasons,
+  } = mediaItem;
+
+  const videoSource =
+    type === "MOVIE" ? videoUrl : seasons?.[0]?.episodes?.[0]?.videoUrl;
+
+  if (!videoSource) {
+    return <Text>No playable video found.</Text>;
+  }
+
   return (
-    <View>
-      <Text style={{ color: "white" }}>Media Id: {id}</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <MediaInfo
+        title={title}
+        releaseYear={releaseYear}
+        ageRestriction={ageRestriction}
+        duration={duration}
+        description={description}
+        type={type}
+        nrOfSeasons={seasons?.length}
+      />
+    </SafeAreaView>
   );
 }
